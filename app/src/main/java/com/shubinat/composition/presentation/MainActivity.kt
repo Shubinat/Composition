b952d67476dedab08a574ccfe5c16c2a69e8848e
Shubinat <shubinat729@gmail.com>
2022-11-08 22:00:19 +0300

    Initial commit

---
+++ app/src/main/java/com/shubinat/composition/presentation/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.shubinat.composition.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.shubinat.composition.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
