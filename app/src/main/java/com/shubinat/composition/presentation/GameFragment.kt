50d3aa045b774fa05b8343a1867bcee251a33629
Shubinat <shubinat729@gmail.com>
2022-11-23 22:50:43 +0300

    Added GameViewModelFactory.kt

--- app/src/main/java/com/shubinat/composition/presentation/GameFragment.kt
+++ app/src/main/java/com/shubinat/composition/presentation/GameFragment.kt
@@ -19,11 +19,12 @@ class GameFragment : Fragment() {
 
     private lateinit var level: Level
 
+    private val gameViewModelFactory by lazy {
+        GameViewModelFactory(requireActivity().application, level)
+    }
+
     private val viewModel by lazy {
-        ViewModelProvider(
-            this,
-            ViewModelProvider.AndroidViewModelFactory.getInstance(requireActivity().application)
-        )[GameViewModel::class.java]
+        ViewModelProvider( this, gameViewModelFactory)[GameViewModel::class.java]
     }
 
     private val tvOptions by lazy {
@@ -68,7 +69,6 @@ class GameFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         observeViewModel()
         setClickListenersToOptions()
-        viewModel.startGame(level)
     }
 
     private fun observeViewModel() {
